96599cb813ea7ef0d681b2baf125b67bea8c57b9
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-03-20 10:06:11 +0530

    Create get title and get current url method.java

---
+++ Getting Started with basic Selenium WebDriver methods/get title and get current url method.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DIPANKAR\\Downloads\\chromedriver_win32\\chromedriver.exe" );
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/#/index");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		
+		
+
+	}
+
+}
