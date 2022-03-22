31a1dc3184621767ce603102bbcc484569086c51
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-03-22 09:41:13 +0530

    Create Datatype.java

---
+++ Brash up Java Concepts for Selenium Automation/Datatype.java
@@ -0,0 +1,14 @@
+
+public class Datatype {
+
+	public static void main(String args[])
+	{
+		int a=5;
+		float b=6.5f;
+		char c='a';
+		String name="Dipankar";
+		System.out.println(name);
+		System.out.println(name+"Kundu");
+		System.out.println(a+b+c);
+	}
+}
