d2d5333aba9208174cc76db442d7677de19cc1d0
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-03-25 09:39:30 +0530

    Create ArratListdemo.java

---
+++ Brash up Java Concepts for Selenium Automation/ArratListdemo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Arraylistdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> a=new ArrayList<String>();
+		a.add("Dipankar");
+		a.add("Kundu");
+		a.add("tarakeswar");
+	   System.out.println(a.get(2));
+
+	}
+
+}
