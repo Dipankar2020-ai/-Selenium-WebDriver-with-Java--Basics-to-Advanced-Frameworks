9424c10d32a45c1f4b8131ed982bee5aaf1afd99
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-03-26 10:22:21 +0530

    Create Methods.java

---
+++ Brash up Java Concepts for Selenium Automation/Methods.java
@@ -0,0 +1,15 @@
+
+public class Methods {
+
+	public static void main(String[] args) {
+		Methods m=new Methods();
+		System.out.println(m.add(5,30));
+		
+
+	}
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+
+}
