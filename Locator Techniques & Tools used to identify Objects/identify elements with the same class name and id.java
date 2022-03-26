7a741ca115d7691905b2d0635b63303128a9a964
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-03-26 17:18:04 +0530

    Create identify elements with the same class name and id.java

---
+++ Locator Techniques & Tools used to identify Objects/identify elements with the same class name and id.java
@@ -0,0 +1,25 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DIPANKAR\\Downloads\\chromedriver_win32\\chromedriver.exe" );
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://sso.teachable.com/secure/9521/identity/sign_up/with_email");
+		//driver.findElement(By.id("user_name")).sendKeys("Dipankar Kundu");
+		driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Dipankar");
+		driver.findElement(By.id("user_email")).sendKeys("abc123");
+		driver.findElement(By.id("password")).sendKeys("Dip12");
+		driver.findElement(By.name("commit")).click();
+		//System.out.println(driver.findElement(By.xpath("//input[@class='inline-error']").getText());
+		System.out.println(driver.findElement(By.xpath("(//div[@class='inline-error'])[1]")).getText());
+		
+		
+	}
+
+}
